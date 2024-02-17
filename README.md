9a8caf2dba60400c991cbd0ba1ca6923d35e2d29
itzik lugassy <73710097+itziklugassy@users.noreply.github.com>
2024-02-17 17:01:51 +0200

    first commit

--- README.md
+++ README.md
@@ -37,7 +37,19 @@ If you aren't satisfied with the build tool and configuration choices, you can `
 
 Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
 
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
+You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However, we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
+
+## Car Component Functionality
+
+The Car component displays information about a car, including its name and image. It provides functionality to delete the car from the list and update its name.
+
+### Deleting a Car
+
+Clicking the "Delete" button on a car card will remove the car from the list.
+
+### Updating a Car's Name
+
+You can update a car's name by entering a new name in the input field provided and clicking the "Update" button. The car's name will be updated accordingly.
 
 ## Learn More
 
--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import car1Image from './images/Tesla Model Y Long Range UK 019_tswxae.webp'; // Importing car1 image
+import car2Image from './images/Tesla Model Y Long Range UK 019_tswxae.webp'; // Importing car2 image
+import car3Image from './images/Tesla Model Y Long Range UK 019_tswxae.webp'; // Importing car3 image
+import car4Image from './images/Tesla Model Y Long Range UK 019_tswxae.webp'; // Importing car4 image
 
-function App() {
+// Car Component
+const Car = ({ car, onDelete, onUpdate }) => {
+  const { id, name, image } = car;
+  const [newName, setNewName] = useState(name); // State to hold the new name for updating
+
+  // Function to handle deletion of the car
+  const handleDelete = () => {
+    onDelete(id);
+  };
+
+  // Function to handle updating the car name
+  const handleUpdate = () => {
+    onUpdate(id, newName);
+  };
+
+  // Function to handle changes in the input field for updating the car name
+  const handleChange = (e) => {
+    setNewName(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="col-md-4 mb-3">
+      <div className="card">
+        <img src={image} className="card-img-top" alt={name} />
+        <div className="card-body">
+          <h5 className="card-title">{name}</h5> {/* Displaying the car name as a title */}
+          <input type="text" value={newName} onChange={handleChange} className="form-control mb-2" />
+          <button className="btn btn-danger mr-2" onClick={handleDelete}>Delete</button>
+          <button className="btn btn-primary" onClick={handleUpdate}>Update</button>
+        </div>
+      </div>
     </div>
   );
-}
+};
+
+// App Component
+const App = () => {
+  // State to hold the list of cars
+  const [cars, setCars] = useState([
+    { id: 1, name: 'Car 1', image: car1Image }, // Using imported car1 image
+    { id: 2, name: 'Car 2', image: car2Image }, // Using imported car2 image
+    { id: 3, name: 'Car 3', image: car3Image }, // Using imported car3 image
+    { id: 4, name: 'Tesla Model Y Long Range UK 019', image: car4Image }, // Using imported car4 image
+  ]);
+
+  // Function to delete a car
+  const deleteCar = (id) => {
+    setCars(cars.filter(car => car.id !== id));
+  };
+
+  // Function to update the name of a car
+  const updateCar = (id, newName) => {
+    setCars(cars.map(car =>
+      car.id === id ? { ...car, name: newName } : car
+    ));
+  };
+
+  return (
+    <div className="container mt-5">
+      <h1 className="mb-4">Garage of Cars</h1>
+      <div className="row">
+        {/* Mapping through the list of cars and rendering Car component for each */}
+        {cars.map(car => (
+          <Car
+            key={car.id}
+            car={car}
+            onDelete={deleteCar}
+            onUpdate={updateCar}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default App;
